2540f483be8b5d24e024434b80c0cccd5e3cf3fb
Martin Wang <martinwang7963@gmail.com>
2023-10-03 16:52:36 +0800

    add score

--- TappyPlane/Hud/Hud.cs
+++ TappyPlane/Hud/Hud.cs
@@ -5,12 +5,22 @@ using System;
 
 public partial class Hud : Control
 {
-    [OnReady]
+    [OnReady("MarginContainer/Hud")]
     public Label ScoreLabel;
+
+    [OnReady("/root/GameManager")]
+    public GameManager GameManager;
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         this.InitOnReady();
+        this.GameManager.Connect(GameManager.SignalName.OnSroceUpdated, Callable.From(OnScoreUpdated));
+    }
+
+    private void OnScoreUpdated()
+    {
+        ScoreLabel.Text = this.GameManager.Score.ToString();
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
--- TappyPlane/Main/Main.cs
+++ TappyPlane/Main/Main.cs
@@ -6,20 +6,23 @@ using System;
 public partial class Main : Control
 {
 
-	[OnReady("/root/GameManager")]
-	public GameManager GameManager;
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		this.InitOnReady();
-	}
+    [OnReady("/root/GameManager")]
+    public GameManager GameManager;
+    [OnReady("MarginContainer/HBoxContainer/GameManager")]
+    public Label Highscore;
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        this.InitOnReady();
+        Highscore.Text = this.GameManager.HighScore.ToString();
+    }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-		if (Input.IsActionJustPressed("fly"))
-		{
-			GameManager.LoadGameScene();
-		}
-	}
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+        if (Input.IsActionJustPressed("fly"))
+        {
+            GameManager.LoadGameScene();
+        }
+    }
 }
