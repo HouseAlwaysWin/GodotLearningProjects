d6c61f6f8121373c59761439faf2e917ef4defe7
MartinWang <martinwang7963@gmail.com>
2023-10-07 18:36:00 +0800

    init proj angryAnimal

---
+++ AngryAnimals/GlobalManager/GlobalManager.cs
@@ -0,0 +1,8 @@
+using Godot;
+using System;
+
+public partial class GlobalManager : Node
+{
+	public delegate void OnUpdateDebugLabel(string text);
+	
+}
---
+++ TappyPlane/Background/ScrollingLayer.cs
@@ -0,0 +1,38 @@
+using Godot;
+using GodotCsharpExtension;
+using GodotCsharpExtension.Attributes;
+using System;
+
+public partial class ScrollingLayer : ParallaxLayer
+{
+
+    [OnReady]
+    public Sprite2D Sprite2D;
+
+    [Export]
+    public Texture2D Texture;
+
+    [Export]
+    public float ScrollScale = 0f;
+
+    [Export]
+    public float TxX = 1920;
+    [Export]
+    public float TxY = 1080;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        this.InitOnReady();
+        MotionScale.SetXPosition(ScrollScale);
+        var scaleF = GetViewportRect().Size.Y / TxY;
+        this.Sprite2D.Texture = Texture;
+        this.Sprite2D.Scale = new Vector2(scaleF, scaleF);
+        MotionMirroring.SetXPosition(TxX * scaleF);
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+    }
+}
