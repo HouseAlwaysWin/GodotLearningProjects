c3bda81f54d999a482a7165fbe6cb65ebbf17e6c
MartinWang <martinwang7963@gmail.com>
2024-03-24 21:39:52 +0800

    update

--- Dungeon RPG/Scripts/Characters/Players/Player.cs
+++ Dungeon RPG/Scripts/Characters/Players/Player.cs
@@ -3,12 +3,21 @@ using System;
 
 public partial class Player : CharacterBody3D
 {
+    private Vector2 direction = new();
     public override void _PhysicsProcess(double delta)
     {
-        base._PhysicsProcess(delta);
+        Velocity = new(direction.X, 0, direction.Y);
+        Velocity *= 5;
+        MoveAndSlide();
     }
 
     public override void _Input(InputEvent @event)
     {
+        direction = Input.GetVector(
+            "MoveLeft",
+            "MoveRight",
+            "MoveForward",
+            "MoveBackward"
+        );
     }
 }
