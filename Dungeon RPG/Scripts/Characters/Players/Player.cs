d8055a747946878ff11c49668fc609808c9c7fbf
MartinWang <martinwang7963@gmail.com>
2024-03-24 21:19:02 +0800

    update

--- Dungeon RPG/Scripts/Characters/Players/Player.cs
+++ Dungeon RPG/Scripts/Characters/Players/Player.cs
@@ -3,5 +3,12 @@ using System;
 
 public partial class Player : CharacterBody3D
 {
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+    }
 
+    public override void _Input(InputEvent @event)
+    {
+    }
 }
