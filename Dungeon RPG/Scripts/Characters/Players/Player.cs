4d0dd6cdfe9a5d7ee4f3912c84e2b2556ce02e17
MartinWang <martinwang7963@gmail.com>
2024-03-25 22:16:22 +0800

    update

--- Dungeon RPG/Scripts/Characters/Players/Player.cs
+++ Dungeon RPG/Scripts/Characters/Players/Player.cs
@@ -3,21 +3,50 @@ using System;
 
 public partial class Player : CharacterBody3D
 {
+    [ExportGroup("Required Node")]
+    [Export] private AnimationPlayer animationPlayer;
+    [Export] private Sprite3D sprite3D;
     private Vector2 direction = new();
+
+    public override void _Ready()
+    {
+        animationPlayer.Play(GameConstants.ANIM_IDLE);
+    }
+
     public override void _PhysicsProcess(double delta)
     {
         Velocity = new(direction.X, 0, direction.Y);
         Velocity *= 5;
         MoveAndSlide();
+        Flip();
     }
 
     public override void _Input(InputEvent @event)
     {
         direction = Input.GetVector(
-            "MoveLeft",
-            "MoveRight",
-            "MoveForward",
-            "MoveBackward"
+            GameConstants.INPUT_MOVE_LEFT,
+            GameConstants.INPUT_MOVE_RIGHT,
+            GameConstants.INPUT_MOVE_FORWARD,
+            GameConstants.INPUT_MOVE_BACKWARD
         );
+
+        if (direction == Vector2.Zero)
+        {
+            animationPlayer.Play(GameConstants.ANIM_IDLE);
+        }
+        else
+        {
+            animationPlayer.Play(GameConstants.ANIM_MOVE);
+        }
+    }
+
+    private void Flip()
+    {
+        bool isNotMovingHorizontally = Velocity.X == 0;
+
+        if (isNotMovingHorizontally) return;
+
+        bool isMovingLeft = Velocity.X < 0;
+        sprite3D.FlipH = isMovingLeft;
     }
 }
---
+++ Dungeon RPG/Scripts/General/GameConstants.cs
@@ -0,0 +1,10 @@
+
+public class GameConstants
+{
+    public const string ANIM_IDLE = "Idle";
+    public const string ANIM_MOVE = "Move";
+    public const string INPUT_MOVE_LEFT = "MoveLeft";
+    public const string INPUT_MOVE_RIGHT = "MoveRight";
+    public const string INPUT_MOVE_FORWARD = "MoveForward";
+    public const string INPUT_MOVE_BACKWARD = "MoveBackward";
+}
