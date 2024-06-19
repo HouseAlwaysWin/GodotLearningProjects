2df0a59501f58431842bcc9cb1fad8060089fa6d
Martin Wang <martinwang7963@gmail.com>
2024-06-19 17:25:18 +0800

    update

--- JumperJam/scripts/player.gd
+++ JumperJam/scripts/player.gd
@@ -3,13 +3,21 @@ extends CharacterBody2D
 var speed = 300.0
 
 var viewport_size
+var gravity = 15.0
+var max_fall_velocity = 1000
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	viewport_size = get_viewport_rect().size
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	pass
+	
+func _physics_process(delta):
+	velocity.y += gravity
+	if velocity.y > max_fall_velocity:
+		velocity.y = max_fall_velocity
+		
 	var direction = Input.get_axis("move_left","move_right")
 	if direction:
 		velocity.x = direction * speed
