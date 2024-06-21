711588c259b9e6093fb559f17aba97199ec993e0
Martin Wang <martinwang7963@gmail.com>
2024-06-21 14:45:55 +0800

    update enemy attack

--- Dungeon RPG/Scripts/Characters/Enemy/EnemyAttackState.cs
+++ Dungeon RPG/Scripts/Characters/Enemy/EnemyAttackState.cs
@@ -1,11 +1,17 @@
 using Godot;
 using System;
+using System.Linq;
 
 public partial class EnemyAttackState : EnemyState
 {
+    private Vector3 targetPosition;
     protected override void EnterState()
     {
         characterNode.AnimationPlayer.Play(GameConstants.ANIM_ATTACK);
+
+        Node3D target = characterNode.AttackAreaNode.GetOverlappingBodies().First();
+        targetPosition = target.GlobalPosition;
+
         characterNode.ChaseAreaNode.BodyExited += HandleChaseAreaBodyExited;
         characterNode.AttackAreaNode.BodyExited += HandleChaseAreaBodyExited;
     }
@@ -14,6 +20,12 @@ public partial class EnemyAttackState : EnemyState
     {
         characterNode.ChaseAreaNode.BodyExited -= HandleChaseAreaBodyExited;
         characterNode.AttackAreaNode.BodyExited -= HandleChaseAreaBodyExited;
+    }
+
+    private void PerformHit()
+    {
+        characterNode.ToggleHitbox(false);
+        characterNode.HitboxNode.GlobalPosition = targetPosition;
     }
 
 }
