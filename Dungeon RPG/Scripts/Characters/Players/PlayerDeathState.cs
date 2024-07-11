5bec5dda5dbe66a837e991b6d260d32fc05d9833
Martin Wang <martinwang7963@gmail.com>
2024-07-11 17:23:07 +0800

    update

--- Dungeon RPG/Scripts/Characters/Players/PlayerDeathState.cs
+++ Dungeon RPG/Scripts/Characters/Players/PlayerDeathState.cs
@@ -3,5 +3,14 @@ using System;
 
 public partial class PlayerDeathState : PlayerState
 {
+    public override void _EnterTree()
+    {
+        characterNode.AnimationPlayer.Play(GameConstants.ANIM_DEATH);
+        characterNode.AnimationPlayer.AnimationFinished += HandleAnimationFinished;
+    }
 
+    private void HandleAnimationFinished(StringName animName)
+    {
+        characterNode.QueueFree();
+    }
 }
