28f7d8a2f2319948477797e097b0328d3a3c9126
Martin Wang <martinwang7963@gmail.com>
2024-08-13 16:42:49 +0800

    add player move

--- FoxAntics/player/Player.cs
+++ FoxAntics/player/Player.cs
@@ -2,6 +2,8 @@ using Godot;
 using GodotCsharpExtension;
 using GodotCsharpExtension.Attributes;
 using System;
+using System.Runtime.CompilerServices;
+using System.Text.Json.Serialization;
 
 public partial class Player : CharacterBody2D
 {
@@ -15,6 +17,10 @@ public partial class Player : CharacterBody2D
     public AnimationPlayer AnimationPlayer;
 
     private const float GRAVITY = 300f;
+    private const float RUN_SPEED = 100f;
+    private const float JUMP_VELOCITY = -400f;
+    private const float MAX_FALL = 400f;
+    private const float HURT_TIME = 0.3f;
 
     public override void _Ready()
     {
@@ -27,7 +33,31 @@ public partial class Player : CharacterBody2D
         {
             float y = Velocity.Y + (GRAVITY * (float)delta);
             Velocity = new Vector2(Velocity.X, y);
-            MoveAndSlide();
         }
+
+        GetInput();
+        MoveAndSlide();
+    }
+
+    public void GetInput()
+    {
+        Velocity = new Vector2(0, Velocity.Y);
+        if (Input.IsActionPressed("jump") && IsOnFloor())
+        {
+            Velocity = new Vector2(Velocity.X, JUMP_VELOCITY);
+        }
+
+        if (Input.IsActionPressed("left"))
+        {
+            Velocity = new Vector2(-RUN_SPEED, Velocity.Y);
+        }
+        if (Input.IsActionPressed("right"))
+        {
+            Velocity = new Vector2(RUN_SPEED, Velocity.Y);
+        }
+
+        var y = Mathf.Clamp(Velocity.Y, JUMP_VELOCITY, MAX_FALL);
+        Velocity = new Vector2(Velocity.X, y);
+
     }
 }
